1afbdb83f85e9624684cfb0ccfe812b199b598a4
Oğuzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2020-09-13 14:36:04 +0300

    Updates

---
+++ precache-manifest.ee9d74ee29d721cdf781e3688a57c619.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56b8f5b0ddf5a93e50c1b92801a8f095",
+    "url": "/movies-react-spa/index.html"
+  },
+  {
+    "revision": "d366987944642b6d29bf",
+    "url": "/movies-react-spa/static/css/main.a048b582.chunk.css"
+  },
+  {
+    "revision": "70e2499cbe03df5fd4cd",
+    "url": "/movies-react-spa/static/js/2.653da53b.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/movies-react-spa/static/js/2.653da53b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d366987944642b6d29bf",
+    "url": "/movies-react-spa/static/js/main.8c06326f.chunk.js"
+  },
+  {
+    "revision": "0360c1e5c0e8b4bfccc4",
+    "url": "/movies-react-spa/static/js/runtime-main.d5c40680.js"
+  }
+]);
